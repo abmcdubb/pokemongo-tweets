48d5045aba4bf075169f73a8d9378965440e22bf
abmcdubb <abmcwilliams@gmail.com>
2016-08-14 21:13:20 -0400

    working event stream, needs styling

--- pokemon_stream.rb
+++ pokemon_stream.rb
@@ -5,45 +5,69 @@ require 'tweetstream'
 require 'pry'
 
 set :server, 'thin'
+connections = []
+tracked_terms = ['Pikachu', 'Bulbasaur']
+all_pokemon = YAML.load_file('list_of_pokemon.yml')
+
+twitter_config = YAML.load_file('config.yml')['twitter']
 
 TweetStream.configure do |config|
-  config.consumer_key       = 'NdgZeu4tT2MDIFQzd8v2WHQ62'
-  config.consumer_secret    = 'eFvcz52ecK4zsrDgQDz5QFTE0f2Xxh0S9ThSyh5NLPE79cnojd'
-  config.oauth_token        = '31439619-5SBfSSz03mAHpkWTyAE8KxMbs3kGJzB73IlDO3PPS'
-  config.oauth_token_secret = 'VQo28ZarlURy8VBO04vQBmcil94oBJwcjQFMcWuHwyZME'
+  config.consumer_key       = twitter_config['consumer_key']
+  config.consumer_secret    = twitter_config['consumer_secret']
+  config.oauth_token        = twitter_config['oauth_token']
+  config.oauth_token_secret = twitter_config['oauth_token_secret']
   config.auth_method        = :oauth
-end
-
-# get '/:hashtag', provides: 'text/event-stream' do
-#   stream :keep_open do |out|
-#     out << random_tweet_about(params[:hashtag])
-#   end
-# end
-
-# def random_tweet_about(hashtag)
-#   TweetStream::Client.new.track(hashtag) do |tweet|
-#     # binding.pry
-#     return tweet.text
-#   end
-# end
-
-#maybe user yield
-get '/tweets' do
-  content_type 'text/event-stream'
-  stream :keep_open do |out|
-    EventMachine::PeriodicTimer.new(1) do
-      TweetStream::Client.new.track('Pikachu') do |tweet|
-        out << { data: { message: tweet.text } }
-      end
-    end
-  end
 end
 
 get '/' do
   haml :index
 end
 
-# all_pokemon = YAML.load_file('list_of_pokemon.yml')
+get '/tweets', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    connections << out
+    out.callback { connections.delete(out) }
+  end
+end
+
+def stream_message(tweet, terms)
+  pokemons = matched_pokemons(tweet.text, terms)
+
+  if pokemons.any?
+    message = "data:{\"message\":#{tweet.text.inspect},\"user\":\"#{tweet.user.name}\""
+    message += ",\"pokemons\":{"
+    pokemons.each { |pokemon| message += pokemon_info(pokemon) }
+    message += "}}\n\n"
+    message
+  end
+end
+
+def pokemon_info(pokemon)
+  all_pokemon = YAML.load_file('list_of_pokemon.yml')
+  
+  "\"#{pokemon}\":\"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/#{all_pokemon[pokemon]['Number']}.png\""
+end
+
+def matched_pokemons(text, terms)
+  terms.select { |term| text.downcase.match(term.downcase)}
+end
+
+EM.schedule do
+  TweetStream::Client.new.track(*tracked_terms) do |tweet|
+    connections.each do |out| 
+      if !tweet.retweet? && tweet.lang == 'en'
+        out << stream_message(tweet, tracked_terms)
+      end
+    end
+  end
+end
+
+
+
+# all_pokemon.keys #names of all pokemon
+
+
+all_pokemon = YAML.load_file('list_of_pokemon.yml')
 
 # http://stackoverflow.com/questions/3669674/streaming-data-from-sinatra-rack-application
 # https://gist.github.com/maccman/2992949
--- public/index.js
+++ public/index.js
@@ -1,15 +1,10 @@
 var source = new EventSource('/tweets');
 
-// source.onmessage = function (event) {
-//   console.log(event)
-//   log.innerText += '\n' + event.data;
-// }, false);
-console.log('STARTING!')
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log('READY!')
   source.onmessage = function(event) {
-    console.log(event)
-    log.innerText += '\n' + event.data;
+    console.log(event.data)
+    data = JSON.parse(event.data)
+    log.innerText += '\n' + data.message;
   };
 })
 
